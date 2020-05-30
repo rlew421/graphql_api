39851f5b1ae8df87915c41bcdce6e9f32195c578
Rachel Lew <rlew07@gmail.com>
2020-05-30 15:06:52 -0600

    Fix typo

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -6,7 +6,7 @@ module Types
       User.all
     end
 
-    field :user, Types::Usertype, null: false do
+    field :user, Types::UserType, null: false do
       argument :id, ID, required: true
     end
 
