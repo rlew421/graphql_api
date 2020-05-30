be276863468ef0d14ee4048fb6b544d02d4f2e17
Rachel Lew <rlew07@gmail.com>
2020-05-30 17:23:44 -0600

    Update seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,19 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-100.times do
-  User.create(
-    name: Faker::Name.name,
-    email: Faker::Internet.email
-  )
-  Book.create(
-    title: Faker::Book.title
-  )
-end
+# 100.times do
+#   User.create(
+#     name: Faker::Name.name,
+#     email: Faker::Internet.email
+#   )
+#   Book.create(
+#     title: Faker::Book.title
+#   )
+# end
+
+user_1 = User.create(name: 'Rachel', email: 'rlew07@gmail.com')
+user_1.books.create(title: 'The Picture of Dorian Gray')
+user_1.books.create(title: 'Norwegian Wood')
+user_1.books.create(title: 'Dune')
+user_1.books.create(title: "Ender's Game")
+user_1.books.create(title: 'Ready Player One')
